ccf4b8fd1676d67fad1c0a77ae5a9f42bb8a318f
Castor6 <731426007@qq.com>
2026-09-19 23:41:20 +0800

    fix: 顶部 Tab 与 Logo 保持固定间距

--- web/src/components/MobileHeader.tsx
+++ web/src/components/MobileHeader.tsx
@@ -33,20 +33,20 @@ const MobileHeader = (props: Props) => {
   return (
     <div
       className={cn(
-        "sticky top-0 pt-3 pb-2 sm:pt-2 px-4 sm:px-6 sm:mb-1 bg-background bg-opacity-80 backdrop-blur-lg grid grid-cols-[minmax(0,1fr)_auto_minmax(0,1fr)] sm:flex sm:justify-between items-center w-full h-auto flex-nowrap shrink-0 z-1",
+        "sticky top-0 pt-3 pb-2 sm:pt-2 px-4 sm:px-6 sm:mb-1 bg-background bg-opacity-80 backdrop-blur-lg flex gap-4 items-center w-full h-auto flex-nowrap shrink-0 z-1",
         offsetTop > 0 && "shadow-md",
         className,
       )}
     >
       {!sm && (
         <>
-          <div className="min-w-0 justify-self-start">
+          <div className="min-w-0">
             <NavigationDrawer />
           </div>
-          <Navigation mobileTabs />
+          <Navigation mobileTabs className="shrink-0" />
         </>
       )}
-      <div className={cn("flex min-w-0 items-center justify-end", sm && "w-full")}>{children}</div>
+      <div className={cn("ml-auto flex shrink-0 items-center justify-end", sm && "w-full")}>{children}</div>
     </div>
   );
 };
